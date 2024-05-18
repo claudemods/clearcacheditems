8ea083a9b462673ac071878eef731f4ab37c5b92
Claudemods <73653396+claudemods@users.noreply.github.com>
2024-05-19 00:09:20 +0100

    Add files via upload

---
+++ scripts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get clean
+sudo rm -rf /var/cache/
+sudo rm -rf /tmp
+sudo rm -rf /root/.cache/
+sudo rm -rf /home/lancaster/.local/share/Trash
+sudo rm -rf /root/.local/share/Trash/
